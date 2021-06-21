35a13be3af0fe59b651d6073269f2512b1ac6c1e
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-21 08:37:09 +0530

    Update benefits.js

--- benefits.js
+++ benefits.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { FaArrowCircleRight} from "react-icons/fa";
+import { FaRegHandPointRight} from "react-icons/fa";
 import './App.css';
 import imageBenefits from './benefitsImage.jpg';
 
@@ -7,72 +7,68 @@ import imageBenefits from './benefitsImage.jpg';
 class App extends Component {
   render() {
   return (
-          
-    <div classname="benefits">
-         
-          <div classname="textBenefits">
-                <div classnane="titleBenefits">
-                          <p>
-                          Going Virtual With Tech
-                          </p>
-                          <p classname="subtextTitleBenefits">
-                          Untap your business potential with wide variety of test support 
-                          suitable for every budget
-                          </p>
-                </div>
-            <div classname="pointsBenefits">
-                  <table classname="rowPointsBenefits">
-                        <tr>  
-                          <td>
-                            <FaArrowCircleRight classname="iconPointsBenefits"/>
-                            </td>
-                            <td>
-                                <p classname="pointsBenefits">
-                                It is an investement for your business
-                                </p>
-                                <p
-                                classname="descriptionPointsBenefits">
-                                Use this space to describe this benefit
-                              </p>
-                            </td>
-                        </tr>
-                    </table>
-                    <table classname="rowPointsBenefits">
-                        <tr>  
-                          <td>
-                            <FaArrowCircleRight classname="iconPointsBenefits"/>
-                            </td>
-                            <td>
-                                <p classname="pointsBenefits">
-                                It is an investement for your business
-                                </p>
-                                <p
-                                classname="descriptionBenefits">
-                                Use this space to describe this benefit
-                              </p>
-                            </td>
-                        </tr>
-                    </table>
-                    <table classname="rowPointsBenefits">
-                        <tr>  
-                          <td>
-                            <FaArrowCircleRight classname="iconPointsBenefits"/>
-                            </td>
-                            <td>
-                                <p classname="pointsBenefits">
-                                It is an investement for your business
-                                </p>
-                                <p
-                                classname="descriptionPointsBenefits">
-                                Use this space to describe this benefit
-                              </p>
-                            </td>
-                        </tr>
-                    </table>
+           <div class="benefits">
+       <div class="imageBenefits">
+          <img src="pexels-maxime-francis-2246476.jpg">
+       </div>
+       <div class="textBenefits">
+        <div classnane="titleBenefits">
+            <h1>
+            Going Virtual With Tech
+            </h1>
+            <p class="subtextTitleBenefits">
+            Untap your business potential with wide variety of test support 
+            suitable for every budget
+            </p>
+        </div>
+        <div class="boxPointsBenefits">
+           <div class="pointsBenefits">
+                 <div class="iconsPointsBenefits">
+                        <i class="far fa-hand-point-right"></i>
+                    </div>
+                    <div class="rowPointsBenefits">
+                          <h2>
+                          It is an investement for your business
+                          </h2>
+                          <p
+                          class="descriptionPointsBenefits">
+                          Use this space to describe this benefit
+                        </p>
+                    </div>
+            </div>
+            <div class="pointsBenefits">
+                <div class="iconsPointsBenefits">
+                       <i class="far fa-hand-point-right"></i>
+                   </div>
+                   <div class="rowPointsBenefits">
+                         <h2>
+                         Connect, meet and rise together
+                         </h2>
+                         <p
+                         class="descriptionPointsBenefits">
+                         Use this space to describe this benefit
+                       </p>
+                   </div>
            </div>
-      </div>
-      </div>
-           );
+           <div class="pointsBenefits">
+            <div class="iconsPointsBenefits">
+                   <i class="far fa-hand-point-right"></i>
+               </div>
+               <div class="rowPointsBenefits">
+                     <h2>
+                     Customized tools for your business management
+                     </h2>
+                     <p
+                     class="descriptionPointsBenefits">
+                     Use this space to describe this benefit
+                   </p>
+               </div>
+       </div>
+    </div>
+       </div>
+   </div>
+   
+        );
 }
 };
 
